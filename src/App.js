a600ff32785b8ffd8b4036462c5617a54738fdd3
Shaivpidadi <shaivpidadi@gmail.com>
2021-09-07 12:26:35 +0530

    Added Loader

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React from "react";
 import { useWeb3React } from "@web3-react/core";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
-import Dashboard from "./views/Dashboard/Dashboard";
-
 import "./App.css";
+import Loader from "./components/Loader/Loader";
 
 const App = () => {
   const { active } = useWeb3React();
@@ -34,7 +33,7 @@ const App = () => {
   }
 
   return (
-    <React.Suspense fallback={<Dashboard />}>
+    <React.Suspense fallback={<Loader />}>
       <BrowserRouter>
         <Switch>{mainContent}</Switch>
       </BrowserRouter>
